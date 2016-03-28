beb4fb19996651b8559e88ad9bc3556bbdce90a2
Robert Limanto <r_limanto@hotmail.com>
2016-03-28 13:21:52 +0800

    test
    test

--- src/Main.java
+++ src/Main.java
@@ -13,8 +13,8 @@ import java.util.Scanner;
 public class Main {
     static DatagramSocket socket = null;
 
-    int port;
-    InetAddress address;
+    static int port;
+    static InetAddress address;
 
     byte[] sendBuf = new byte[256];
 
@@ -23,118 +23,134 @@ public class Main {
 
     static void send(byte[] buf) throws IOException
     {
-        InetAddress address =    InetAddress.getByName("127.0.0.1");
         DatagramPacket packet = new DatagramPacket(buf, buf.length,
-                address, 4445);
+                Main.address, Main.port);
         socket.send(packet);
     }
 
     public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
 
 
-        try{
-            System.out.println("Welcome to the client side");
-            System.out.println("What do you want to do?");
-            System.out.println("1. Read the content of the file.");
-            System.out.println("2. Insert content into a file.");
+        System.out.println("Welcome to the client side");
 
-            System.out.print("Input the choice:");
-            Scanner sc = new Scanner(System.in);
-            int choice = sc.nextInt();
+        System.out.print("Specify server port: ");
+        Main.port = sc.nextInt();
 
-            switch(choice){
-                case 1: System.out.print("Please input the file path:");
-                    sc = new Scanner(System.in);
-                    String filePath  = sc.nextLine();
+        System.out.print("Specify server IP: ");
+        String address = sc.next();
 
-                    System.out.print("Please input the offset:");
-                    sc = new Scanner(System.in);
-                    int offset  = sc.nextInt();
+        Main.address = InetAddress.getByName(address);
 
+        System.out.println();
 
-                    System.out.print("Please input the number of bytes:");
-                    sc = new Scanner(System.in);
-                    int numOfBytes  = sc.nextInt();
+        while(true) {
+            try {
 
-                    readFileContent(filePath, offset, numOfBytes);
-                    break;
+                System.out.println("What do you want to do?");
+                System.out.println("1. Read the content of the file.");
+                System.out.println("2. Insert content into a file.");
+                System.out.println("3. Monitor the client. ");
+                System.out.println("4. Simulate loss transmission.");
+                System.out.println("0. Exit");
+                System.out.print("Input the choice:");
+                int choice = sc.nextInt();
 
-                case 2:
-                    System.out.print("Please input the file path:");
-                    sc = new Scanner(System.in);
-                    filePath  = sc.nextLine();
-
-                    System.out.print("Please input the offset:");
-                    sc = new Scanner(System.in);
-                    offset  = sc.nextInt();
-
-
-                    System.out.print("Please input the bytes to write into the file:");
-                    sc = new Scanner(System.in);
-                    String bytesToWrite  = sc.nextLine();
-
-                    writeFileContent(filePath, offset, bytesToWrite.getBytes());
-                    break;
-
-                case 3:
-                    System.out.print("Please input the file path:");
-                    sc = new Scanner(System.in);
-                    filePath  = sc.nextLine();
-
-                    System.out.print("Please input the offset:");
-                    sc = new Scanner(System.in);
-                    int intervalMilli  = sc.nextInt();
-
-                    monitorFile(filePath, intervalMilli);
-                    break;
-
-            }
-
-            while(true)
-            {
-                byte[] recvBuf = new byte[FileServerThread.MAX_PACKET_BYTES];
-                DatagramPacket packet = new DatagramPacket(recvBuf, recvBuf.length);
-                socket.receive(packet);
-
-                UnMarshaller um = new UnMarshaller(packet.getData());
-                int resType = (byte) um.getNextByte();
-
-                // Handles responses
-                switch (resType)
-                {
-                    case MessageType.RESPONSE_MSG:
-                        // Only normal responses have the sequence numbers embedded
-                        System.out.println("Response Received: " + (String) um.getNext() + " seq num = " + um.getNext());
+                if(choice == 0){
                         break;
-                    case MessageType.RESPONSE_BYTES:
-                        // Only normal responses have the sequence numbers embedded
-                        System.out.println("Bytes received - length: " + new String((byte[])um.getNext()) + " seq num = " + um.getNext());
-                        break;
-                    case MessageType.CALLBACK:
-                        System.out.println("Callback received for " + um.getNext() + " - update length: " + ((byte[]) um.getNext()).length);
-                        break;
-                    case MessageType.ERROR:
-                        System.err.println("Error occured - code " + (int) um.getNext() + ": " + um.getNext());
-                        break;
-                    case MessageType.RESPONSE_PATH:
-                        // Only normal responses have the sequence numbers embedded
-                        String path = (String)um.getNext();
-                        System.out.println("Duplicated file path recvd: " + path + " seq num = " + um.getNext());
-                        // Sends a delete request with the newly gotten file name as the parameter
-                        send(new Marshaller((byte) MessageType.DELETE_FILE, path, sequenceNum++).getBytes());
-                        break;
-                    case MessageType.RESPONSE_SUCCESS:
-                        System.out.println(um.getNext() + " seq num = " + um.getNext());
-                        break;
-                    default:
-                        System.err.println("Strange request received" + um.getNext());
                 }
+                switch (choice) {
+                    case 1:
+                        System.out.print("Please input the file path:");
+                        sc = new Scanner(System.in);
+                        String filePath = sc.nextLine();
+
+                        System.out.print("Please input the offset:");
+                        sc = new Scanner(System.in);
+                        int offset = sc.nextInt();
+
+
+                        System.out.print("Please input the number of bytes:");
+                        sc = new Scanner(System.in);
+                        int numOfBytes = sc.nextInt();
+
+                        readFileContent(filePath, offset, numOfBytes);
+                        break;
+
+                    case 2:
+                        System.out.print("Please input the file path:");
+                        sc = new Scanner(System.in);
+                        filePath = sc.nextLine();
+
+                        System.out.print("Please input the offset:");
+                        sc = new Scanner(System.in);
+                        offset = sc.nextInt();
+
+
+                        System.out.print("Please input the bytes to write into the file:");
+                        sc = new Scanner(System.in);
+                        String bytesToWrite = sc.nextLine();
+
+                        writeFileContent(filePath, offset, bytesToWrite.getBytes());
+                        break;
+
+                    case 3:
+                        System.out.print("Please input the file path:");
+                        sc = new Scanner(System.in);
+                        filePath = sc.nextLine();
+
+                        System.out.print("Please input the offset:");
+                        sc = new Scanner(System.in);
+                        int intervalMilli = sc.nextInt();
+
+                        monitorFile(filePath, intervalMilli);
+                        break;
+
+                }
+
+                while (true) {
+                    byte[] recvBuf = new byte[FileServerThread.MAX_PACKET_BYTES];
+                    DatagramPacket packet = new DatagramPacket(recvBuf, recvBuf.length);
+                    socket.receive(packet);
+
+                    UnMarshaller um = new UnMarshaller(packet.getData());
+                    int resType = (byte) um.getNextByte();
+
+                    // Handles responses
+                    switch (resType) {
+                        case MessageType.RESPONSE_MSG:
+                            // Only normal responses have the sequence numbers embedded
+                            System.out.println("Response Received: " + (String) um.getNext() + " seq num = " + um.getNext());
+                            break;
+                        case MessageType.RESPONSE_BYTES:
+                            // Only normal responses have the sequence numbers embedded
+                            System.out.println("Bytes received - length: " + new String((byte[]) um.getNext()) + " seq num = " + um.getNext());
+                            break;
+                        case MessageType.CALLBACK:
+                            System.out.println("Callback received for " + um.getNext() + " - update length: " + ((byte[]) um.getNext()).length);
+                            break;
+                        case MessageType.ERROR:
+                            System.err.println("Error occured - code " + (int) um.getNext() + ": " + um.getNext());
+                            break;
+                        case MessageType.RESPONSE_PATH:
+                            // Only normal responses have the sequence numbers embedded
+                            String path = (String) um.getNext();
+                            System.out.println("Duplicated file path recvd: " + path + " seq num = " + um.getNext());
+                            // Sends a delete request with the newly gotten file name as the parameter
+                            send(new Marshaller((byte) MessageType.DELETE_FILE, path, sequenceNum++).getBytes());
+                            break;
+                        case MessageType.RESPONSE_SUCCESS:
+                            System.out.println(um.getNext() + " seq num = " + um.getNext());
+                            break;
+                        default:
+                            System.err.println("Strange request received" + um.getNext());
+                    }
+                }
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-
-
-        }
-        catch(Exception e){
-            e.printStackTrace();
         }
     }
 
